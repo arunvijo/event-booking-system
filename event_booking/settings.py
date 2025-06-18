d6f04f3e3b6c4f9d86cae2314b9011118d9b8f56
arunvijo <arunvijo2004@gmail.com>
2025-06-18 15:40:10 +0530

    Fix: updated settings file

--- event_booking/settings.py
+++ event_booking/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
     'core.apps.CoreConfig',
 ]
